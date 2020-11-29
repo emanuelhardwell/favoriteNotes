80c43cd9a238fa31da998a757c8deeab1cd74471
emanuelhardwell <emanuelnoe16@gmail.com>
2020-11-29 16:55:49 -0600

    Model of user created

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+/*  */
+/*  */
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { Schema } = mongoose();
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
