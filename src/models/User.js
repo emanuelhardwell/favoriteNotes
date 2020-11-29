d57f1956719411651dbc5db4e3164a0b914214dd
emanuelhardwell <emanuelnoe16@gmail.com>
2020-11-29 17:29:00 -0600

    Module and view signup created

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@
 /*  */
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const { Schema } = mongoose();
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,13 +2,48 @@
 /*  */
 const express = require("express");
 const router = express.Router();
-
-router.get("/user/signin", (req, res) => {
-  res.send("Hola estas en signin");
-});
+const User = require("../models/User");
 
 router.get("/user/signup", (req, res) => {
-  res.send("Hola estas en signup");
+  res.render("users/signup");
+});
+
+router.post("/user/signup", async (req, res) => {
+  const { name, email, password, passwordConfirm } = req.body;
+  let errors = [];
+  if (name < 4) {
+    errors.push({ text: "The name is empty" });
+  }
+  if (password != passwordConfirm) {
+    errors.push({ text: "The password is not math" });
+  }
+  if (password < 5) {
+    errors.push({ text: "The password is very small" });
+  }
+  if (errors.length > 0) {
+    res.render("users/signup", {
+      errors,
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+  } else {
+    const searchEmail = await User.findOne({ email: email });
+    if (searchEmail) {
+      req.flash("errorMessage", "The email is already associated");
+      res.redirect("/user/signup");
+    }
+    const newUser = new User({ name, email, password });
+    newUser.password = await newUser.encryptPassword(password);
+    await newUser.save();
+    req.flash("successMessage", "You are registered successfully");
+    res.redirect("/user/signin");
+  }
+});
+
+router.get("/user/signin", (req, res) => {
+  res.render("users/signin");
 });
 
 module.exports = router;
